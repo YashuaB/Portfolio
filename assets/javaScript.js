ddd4c8e919e3d5460dd5405a0ebd5dfbd4e2422b
yashua <yashuab.abraham@gmail.com>
2019-12-26 00:53:13 -0500

    create border for text intro. removed other elements from the page

--- assets/javaScript.js
+++ assets/javaScript.js
@@ -1,1 +1,14 @@
 
+$("li").click( function(){
+  
+ let id = $(this).attr("id")
+  if(id === "about-button"){
+    $(".about").fadeIn(3000)
+
+} else if (id === "contact-button"){
+    $(".contact-container").fadeIn(3000)
+
+} else if (id === "work-button"){
+  $(".carousel-background").fadeIn(3000)
+}
+})
