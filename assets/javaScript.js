b3719fe9cc4ef5f91b86ce7f51fdd01359a11a04
yashua <yashuab.abraham@gmail.com>
2020-01-13 02:00:59 -0500

    removed gradient from background, center and resize about section, and created transition for closing each section

--- assets/javaScript.js
+++ assets/javaScript.js
@@ -1,14 +1,29 @@
 
 $("li").click( function(){
-  
+// fading out main elements on home page
+  $(".nav-intro-container").fadeOut(1000)
+  $(".logo").fadeOut(2000)
+  $(".social-icons").fadeOut(1000)
+
  let id = $(this).attr("id")
   if(id === "about-button"){
+    $(".about").css({"margin-top":"0"})
     $(".about").fadeIn(3000)
 
 } else if (id === "contact-button"){
+  $(".contact").css({"margin":"0"})
     $(".contact-container").fadeIn(3000)
 
 } else if (id === "work-button"){
   $(".carousel-background").fadeIn(3000)
 }
+})
+
+$(".close-icon").click( function(){
+    
+  // $(".closed").css({"display":"none"})
+  $(".nav-intro-container").fadeIn(3000)
+  $(".logo").fadeIn(3000)
+  $(".social-icons").fadeIn(3000)
+  $(".closed").fadeOut(2000)
 })
